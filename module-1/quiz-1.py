62f56189e0fbda39a015073dc5120dfc877485a8
vaniaelvina <vaniaelvinaa@gmail.com>
2024-12-19 16:27:20 +0700

    reorganize

---
+++ module-1/quiz-1.py
@@ -0,0 +1,120 @@
+
+'''
+
+Question 1
+Read the description given by
+
+help(round)
+
+Then select the function call(s) below that run without error. You can check your answers by running the code in the Python Shell.
+
+answers:
+round(45)
+round(45.345,2)
+round(45.8)
+
+Question 2
+What type of value does built-in function id return? Determine the answer using the description given by
+
+help(id)
+
+answer:
+int
+
+Question 3
+Consider this code:
+x = 12 / 3
+What value does x refer to?
+
+answer:
+4.0
+
+Question 4
+Consider this code:
+x = 12 // 3
+What value does x refer to?
+
+answer: 
+4
+
+Question 5
+Consider this code:
+x = 3
+y = 5
+x = y
+After the code above has executed, what value does x refer?
+
+answer: 
+5
+
+Question 6
+Consider this code:
+x = 3
+y = 5
+x = y
+
+After the code above has executed, what value does y refer?
+
+answer:
+5
+
+Question 7
+Consider this code:
+apple = banana
+When the code above is executed, what type of error occurs?
+
+answer:
+NameError
+
+Question 8
+Consider this code:
+def f(data):
+    return data * 0.5
+
+Select the phrase that describes data.
+
+answer:
+a parameter
+
+Question 9
+def g(a, b):
+    c = a + b
+    return c
+
+How many parameters does function g have?    
+
+answer:
+2
+
+Question 10
+Consider this code:
+value = 8.564
+result = round(value)
+
+Select the phrase that describes value in the second line.
+
+answer:
+an argument
+
+Question 11
+Consider this code:
+round(45.342)
+
+What value does the expression above produce?
+
+answer:
+45
+
+Question 12
+Consider this code:
+def bigger(x):
+    return x ** x
+
+bigger(12)
+
+Which value does bigger(12) produce?
+
+answer:
+8916100448256
+
+'''
